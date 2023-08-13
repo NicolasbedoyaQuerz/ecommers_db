f16f7f4a1e847abe102e4ca8eb5effcc7ca77038
Nicolas Bedoya <nicolasbedoya32@gmail.com>
2023-08-13 17:35:45 -0500

    subiendo a render

--- src/config/config.js
+++ src/config/config.js
@@ -17,7 +17,7 @@ module.exports = {
     host: "127.0.0.1",
     dialect: "postgres",
   },
-  "production": {
+  production: {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
