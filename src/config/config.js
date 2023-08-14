1f78808a0db0c496b329c071bf845eee8827b40e
Nicolas Bedoya <nicolasbedoya32@gmail.com>
2023-08-13 22:38:56 -0500

    subiendo a render

--- src/config/config.js
+++ src/config/config.js
@@ -1,23 +1,23 @@
 require('dotenv').config()
 
 module.exports = {
-  development: {
-    username: "postgres",
-    password: "1234",
-    database: "ecommers_db",
-    port: 5432,
-    host: "localhost",
-    dialect: "postgres",
-    logging: false,
+  "development": {
+    "username": "postgres",
+    "password": "1234",
+    "database": "ecommers_db",
+    "port": 5432,
+    "host": "localhost",
+    "dialect": "postgres",
+    "logging": false,
   },
-  test: {
-    username: "root",
-    password: null,
-    database: "ecommers_db",
-    host: "127.0.0.1",
-    dialect: "postgres",
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "ecommers_db",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
   },
-  production: {
+  "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
